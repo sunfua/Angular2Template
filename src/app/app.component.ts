216482683be847ef637412e4a6db4715fb7867cc
sunfua <sunfua@gmail.com>
2017-02-20 11:11:04 +0700

    add new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1>
+  <my-tutorial></my-tutorial>
+  `,
 })
 export class AppComponent  { name = 'Angular'; }
---
+++ src/app/tutorial.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-tutorial',
+    template: '<h2>Tutorial Component</h2>'
+})
+export class TutorialComponent{}
